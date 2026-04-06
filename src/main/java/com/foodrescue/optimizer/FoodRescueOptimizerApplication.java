0d99b5daa06af0149355f000db3b2acc71b6718a
Erdkash1 <erdkash1@gmail.com>
2026-04-06 10:21:54 -0500

    initial project setup with docker and database config

---
+++ src/main/java/com/foodrescue/optimizer/FoodRescueOptimizerApplication.java
@@ -0,0 +1,13 @@
+package com.foodrescue.optimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodRescueOptimizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodRescueOptimizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/foodrescue/optimizer/FoodRescueOptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodrescue.optimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodRescueOptimizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
