6a2c53315373628b73cc7cc8e38331d311563583
Erdkash1 <erdkash1@gmail.com>
2026-04-07 15:16:19 -0500

    add testcontainers repository test

---
+++ src/test/java/com/foodrescue/optimizer/repository/DonorRepositoryTest.java
@@ -0,0 +1,52 @@
+package com.foodrescue.optimizer.repository;
+
+
+import com.foodrescue.optimizer.domain.Donor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class DonorRepositoryTest {
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            DockerImageName.parse("postgis/postgis:16-3.4")
+                    .asCompatibleSubstituteFor("postgres")
+    );
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Autowired
+    private DonorRepository donorRepository;
+
+    @Test
+    void whenDonorIsSaved_thenItShouldHaveAnId(){
+        Donor donor = Donor.builder()
+                .name("Test Donor")
+                .email("test@test.com")
+                .latitude(40.7128)
+                .longitude(-74.0060)
+                .build();
+
+        Donor savedDonor = donorRepository.save(donor);
+
+        assertThat(savedDonor.getId()).isNotNull();
+
+    }
+}
