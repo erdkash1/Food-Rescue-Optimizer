69d44894742b0b2d8c50993bd2801c41c2c28352
Erdkash1 <erdkash1@gmail.com>
2026-04-11 22:24:57 -0500

    add RecipientService

---
+++ src/main/java/com/foodrescue/optimizer/service/RecipientService.java
@@ -0,0 +1,29 @@
+package com.foodrescue.optimizer.service;
+
+import com.foodrescue.optimizer.domain.Recipient;
+import com.foodrescue.optimizer.repository.RecipientRepository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class RecipientService {
+    private final RecipientRepository recipientRepository;
+
+    public Recipient createRecipient(Recipient recipient){
+        return recipientRepository.save(recipient);
+    }
+    public List<Recipient> getAllRecipient(){
+        return recipientRepository.findAll();
+    }
+    public Optional<Recipient> getRecipientById(UUID id){
+        return recipientRepository.findById(id);
+    }
+
+
+}
