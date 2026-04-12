d65073512e2c25e37088578d39958d46108125fa
Erdkash1 <erdkash1@gmail.com>
2026-04-11 22:51:38 -0500

    add VehicleService to service package

---
+++ src/main/java/com/foodrescue/optimizer/service/VehicleService.java
@@ -0,0 +1,20 @@
+package com.foodrescue.optimizer.service;
+
+import com.foodrescue.optimizer.domain.Vehicle;
+import com.foodrescue.optimizer.repository.VehicleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class VehicleService {
+    private final VehicleRepository vehicleRepository;
+
+    public Vehicle createVehicle(Vehicle vehicle){ return vehicleRepository.save(vehicle);}
+    public List<Vehicle> getAllVehicle(){ return vehicleRepository.findAll();}
+    public Optional<Vehicle> getVehicleById(UUID id){ return vehicleRepository.findById(id);}
+
+}
